eb06695a6f54b5664038674f26875563c50db63b
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-10 13:03:51 +0200

    milestone 3

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -1,7 +1,7 @@
 /* 
- Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 var app = new Vue({
@@ -13,8 +13,9 @@ var app = new Vue({
     index_dir:'0',  //uso index_dir come variabile per mostrare i messaggi di uno specifico oggetto. cliccando l'elemento li dentro l'ul "contact-list" prendo il numero della posizione e lo assegno al ciclo for dentro i messaggi
     user:{
       name:'Nome utente',
-      avatar: '_io'
-    },
+      avatar: '_io',
+      messages:[],  //creo un array vuoto contente i messaggi che verranno inseriti dall'utente trampite l'input del footer
+    },    
     contacts: [
       {
         name: 'Michele',
@@ -102,11 +103,19 @@ var app = new Vue({
     ]
   },
   methods: {
-    addImageToAvatar(image){
+    addImageToAvatar(image){  //funzione che mi aggiunge la proprieta avatar del singolo oggetto dei contact dentro la stringa del source dell'immagine
       return `img/avatar${image}.jpg`;      
-    },    
+    },
+    sendText(str){  //funzione che viene richiamato in riga 161 dell'html dove appena premao invio , il testo dell'input viene pushato nell'array messages dentro l'oggetto user
+      const message = {
+        text: ''
+      };
+      message.text = str;
+      this.user.messages.push(message.text);
+      this.text = ''
+    }
   },computed: {
-    filterContacts(){
+    filterContacts(){ //filtro per i contatti nella sezione aside.left dell'html
       return this.contacts.filter((element) => {
         return element.name.toLowerCase().includes(this.search.toLowerCase());
       })
