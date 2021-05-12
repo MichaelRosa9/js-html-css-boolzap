91e4202f6b01bdd27fe9a655c170a2f59ccec3f3
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-12 10:58:28 +0200

    milestone dropdown menu

--- milestone-5/js/script.js
+++ milestone-5/js/script.js
@@ -8,10 +8,10 @@ Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permett
 var app = new Vue({
   el: '#app',
   data: {
-    search:'',  // uso search per la sezione input dei contatti per poter filtrare il contatto che sto cercando
-    messages_dropdown: false,
+    search:'',  // uso search per la sezione input dei contatti per poter filtrare il contatto che sto cercando    
     text:'',
-    index_dir:'0',  //uso index_dir come variabile per mostrare i messaggi di uno specifico oggetto. cliccando l'elemento li dentro l'ul "contact-list" prendo il numero della posizione e lo assegno al ciclo for dentro i messaggi
+    index_dir:'0',  //uso index_dir come variabile per mostrare i messaggi di uno specifico oggetto. cliccando l'elemento li dentro l'ul "contact-list" prendo il numero della posizione e lo assegno al ciclo for dentro i messaggi  
+    index_dropdown: '0',
     user:{
       name:'Nome utente',
       avatar: '_io',      
@@ -107,7 +107,8 @@ var app = new Vue({
       return this.contacts.filter((element) => {
         return element.name.toLowerCase().includes(this.search.toLowerCase());
       })
-    }
+    },
+    
   },
   methods: {
     addImageToAvatar(image){  //funzione che mi aggiunge la proprieta avatar del singolo oggetto dei contact dentro la stringa del source dell'immagine
@@ -117,26 +118,40 @@ var app = new Vue({
       this.$refs.input.focus();
     },
     sendText(str, index){  //funzione che viene richiamato in riga 161 dell'html dove appena premi invio, il testo dell'input viene pushato nell'array messages dentro l'oggetto indicato con l'index dell'array contacts
-      const message = {
-        date: '10/01/2020 15:50:00',
-        text: '',
-        status: 'sent'
-      }
-      message.text = str;
-      this.filterContacts[index].messages.push(message);
-      this.text = '';
-     
-      //creo un oggetto con il testo 'ok' che richiamero col setTimeout
-      const message_recive = {
-        date: '10/01/2020 15:50:00',
-        text: 'ok',
-        status: 'received'
-      }
-      setTimeout(() => {  //uso il setTimeout per simulare una risposta dopo che l'utente preme Enter per inviare il messaggio
-        this.filterContacts[index].messages.push(message_recive);        
-      }, 1000);
-    }
+      if(str.length > 0){
 
+        const message = {
+          date: dayjs().format('DD/MM/YYYY HH:mm:ss'),//funzione della libreria dayjs che mi indica l'orario esatto
+          text: '',
+          status: 'sent'
+        }
+        message.text = str;
+        this.filterContacts[index].messages.push(message);
+        this.text = '';
+       
+        //creo un oggetto con il testo 'ok' che richiamero col setTimeout
+        setTimeout(() => {  //uso il setTimeout per simulare una risposta dopo che l'utente preme Enter per inviare il messaggio
+          const message_recive = {
+            date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+            text: 'ok',
+            status: 'received'
+          }
+          this.filterContacts[index].messages.push(message_recive);        
+        }, 1000);
+      }
+    },
+    lastAccess(index){
+      let contactMsgs = this.contacts[index].messages;
+      return contactMsgs[contactMsgs.length-1].date;
+    },
+    lastMessage(index){
+      let contactMsgs = this.contacts[index].messages;
+      if(contactMsgs[contactMsgs.length - 1].text.length > 30){
+        let spliceMsg = contactMsgs[contactMsgs.length - 1].text.slice(0, 30) + "...";
+        return spliceMsg;
+      }
+      return contactMsgs[contactMsgs.length-1].text;
+    }
   }
   
 });
